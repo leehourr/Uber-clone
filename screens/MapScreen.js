0970f197ab8d2b868cbffc533f7057b9c1e6e324
Lee Hour <lhour999@gmail.com>
2023-03-20 18:04:18 +0700

    added map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,35 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../store/navSlice";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = () => {
+  const origin = useSelector(selectOrigin);
+  console.log(origin);
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <MapView
+      className="flex-1"
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.05,
+        longtitudeDelta: 0.05,
+      }}
+    >
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+    </MapView>
   );
 };
 
