03fc3f3753adc683e384e23a5bedcb05e5c75afa
Lee Hour <lhour999@gmail.com>
2023-03-20 18:40:06 +0700

    added NavigateCard

---
+++ components/NavigateCard.js
@@ -0,0 +1,56 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useNavigation } from "@react-navigation/native";
+import { store } from "../store/store";
+import { setDestination } from "../store/navSlice";
+
+const NavigateCard = () => {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView className="bg-white flex-1">
+      <Text className="text-center py-5 text-xl">Good Morning, Lee Hour</Text>
+      <View className="border-t border-gray-200 flex-shrink">
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            styles={{
+              container: {
+                backgroundColor: "White",
+                paddingTop: 20,
+                flex: 0,
+              },
+              textInput: {
+                backgroundColor: "#DDDDDF",
+                fontSize: 18,
+              },
+              textInputContainer: {
+                paddingHorizontal: 20,
+                paddingBottom: 0,
+              },
+            }}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              store.dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            enablePoweredByContainer={false}
+            query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,31 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Map from "../components/Map";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/NavigateCard";
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <View>
       <View className="h-1/2">
         <Map />
       </View>
-      <View className="h-1/2"></View>
+      <View className="h-1/2">
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+          {/* <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{ headerShown: false }}
+          /> */}
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };
