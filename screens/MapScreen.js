f5787b5daced9aa166e7fe947095ec8ba380b209
Lee Hour <lhour999@gmail.com>
2023-03-20 18:48:35 +0700

    added rider card

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,27 +3,28 @@ import React from "react";
 import Map from "../components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <View>
-      <View className="h-1/2">
+      <View className="h-[30%]">
         <Map />
       </View>
-      <View className="h-1/2">
+      <View className="h-[70%]">
         <Stack.Navigator>
           <Stack.Screen
             name="NavigateCard"
             component={NavigateCard}
             options={{ headerShown: false }}
           />
-          {/* <Stack.Screen
+          <Stack.Screen
             name="RideOptionsCard"
             component={RideOptionsCard}
             options={{ headerShown: false }}
-          /> */}
+          />
         </Stack.Navigator>
       </View>
     </View>
